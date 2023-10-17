0be717f2b3794e0be90ec767700d1ee0bbd37754
agrueiror <grueiroalex@gmail.com>
2023-10-17 19:49:01 +0200

    sus

--- src/main/java/Controlador/Mar.java
+++ src/main/java/Controlador/Mar.java
@@ -1,12 +1,20 @@
 package Controlador;
 
+import Modelo.Alumno;
 import Modelo.Curso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mar {
     public static void marshall(){
         try {
             Curso primero =new Curso();
             Curso segundo =new Curso();
+            List<Alumno> alumnos=new ArrayList<>();
+            for (int i = 0; i < 24; i++) {
+                alumnos.add(new Alumno());
+            }
         }
     }
 }
